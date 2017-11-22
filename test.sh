3946be2390c1cf891d7e9cbe4678edc5946e620e
Alan Platt <aplatt@equalexperts.com>
2017-11-22 11:53:26 +0000

    add basic tests and test requirements

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+VENV_NAME=".venv"
+if ! [ -e "${VENV_NAME}/bin/activate" ]; then
+  echo "Creating virtualenv..."
+  virtualenv  -p python3 ${VENV_NAME}
+fi
+
+source "${VENV_NAME}/bin/activate"
+
+echo "Installing requirements..."
+pip install -q -r requirements.txt -r requirements-tests.txt
+
+echo "Cleaning .pyc files..."
+find . -iname "*.pyc" -delete
+
+echo "Running tests..."
+python -m pytest tests/
+
+echo "Running flake8..."
+flake8 ee-microdc-init/ tests/
+
+echo "Done!!"
