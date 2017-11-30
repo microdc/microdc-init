892a986c6795c3f0125fdcce72c429e225335054
Alan Platt <aplatt@equalexperts.com>
2017-11-30 13:25:42 +0000

    add yaml_loader to get config yaml

---
+++ microdc/yaml_loader.py
@@ -0,0 +1,25 @@
+import yaml
+from collections import OrderedDict
+
+
+def readyaml(filename):
+    try:
+        file = open(filename)
+        yamldict = ordered_load(file, yaml.SafeLoader)
+        file.close()
+        return yamldict
+    except(IOError, ImportError):
+        raise
+
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
---
+++ tests/test_yaml_loader.py
@@ -0,0 +1,44 @@
+import unittest
+import yaml
+import collections
+from microdc.yaml_loader import (readyaml, ordered_load)
+
+
+class TestYamlLoader(unittest.TestCase):
+
+    def test_readyaml_with_valid_yaml(self):
+        """
+        If valid YAML is loaded then an ordered dict is returned with valid
+        data
+        """
+        result = readyaml('tests/good_config.yaml')
+        self.assertIs(type(result), collections.OrderedDict)
+        self.assertEquals(result['project'], 'testproject')
+        self.assertEquals(result['apps']['helloworld']['port'], 39111)
+
+    def test_readyaml_with_invalid_yaml(self):
+        """
+        If valid arugment is given an ordered dict is returned
+        """
+        self.assertRaises(yaml.scanner.ScannerError,
+                          readyaml,
+                          "tests/bad_config.yaml")
+
+    def test_ordered_load(self):
+        """
+        When given yaml it should come out in the same order
+        """
+        YAML = """
+        apps:
+          one: "test"
+          two: "test"
+          three: "test"
+          four: "test"
+          five: "test"
+        """
+        result = ordered_load(YAML, yaml.SafeLoader)
+        ordered_apps = []
+        for key, value in result['apps'].items():
+            ordered_apps.append(key)
+        self.assertEquals(ordered_apps,
+                          ['one', 'two', 'three', 'four', 'five'])
