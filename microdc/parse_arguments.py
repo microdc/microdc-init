c0d78a3a3624b88265c192e080fc15adbd49b199
Alan Platt <aplatt@equalexperts.com>
2017-11-30 13:22:37 +0000

    add bulk of command line options along with tests

--- microdc/parse_arguments.py
+++ microdc/parse_arguments.py
@@ -7,14 +7,30 @@ def parse_args(argv):
     """
     parser = argparse.ArgumentParser(description='''Generate shell commands from
                                      a yaml config file to setup MicroDC.''')
-    parser.add_argument('--config',
-                        metavar='CONFIG',
-                        nargs=1,
-                        help='The location of th YAML config file')
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('--config',
+                          metavar='CONFIG',
+                          nargs=1,
+                          required=2,
+                          help='The location of the YAML config file')
+    required.add_argument('--env',
+                          metavar='ENV',
+                          nargs=1,
+                          required=2,
+                          help='Which environment to modify')
+    required.add_argument('--stack',
+                          metavar='STACK',
+                          nargs=1,
+                          required=2,
+                          help='Which stack to apply')
     parser.add_argument('--account',
                         metavar='ACCOUNT',
                         nargs=1,
                         default='default',
                         help='Set the AWS account to use')
+    parser.add_argument('action',
+                        metavar='ACTION',
+                        nargs=1,
+                        help='Action to perform: up or dowm')
 
     return parser.parse_args(argv)
--- tests/test_parse_arguments.py
+++ tests/test_parse_arguments.py
@@ -8,17 +8,19 @@ class TestParseArguments(unittest.TestCase):
         """
         If no options are given then we expect a valid arguments object
         """
-        result = parse_args([])
-        self.assertEquals(result.config, None)
-        self.assertEquals(result.account, 'default')
+        with self.assertRaises(SystemExit) as result:
+            parse_args([])
+
+        self.assertEqual(result.exception.code, 2)
 
     def test_parse_args_with_valid_options(self):
         """
         If valid options are given then we expect a valid arguments object
         """
-        result = parse_args(['--config',
-                             '../dir/file.yaml',
-                             '--account',
-                             'nonprod'])
+        result = parse_args(['--config', '../dir/file.yaml',
+                             '--account', 'nonprod',
+                             '--env', 'dev',
+                             '--stack', 'application',
+                             'up'])
         self.assertEquals(result.config[0], '../dir/file.yaml')
         self.assertEquals(result.account[0], 'nonprod')
