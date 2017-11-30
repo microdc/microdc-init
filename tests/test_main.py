8b37736c48e8d0a4240f61bc8bd2e8f31ad4af34
Alan Platt <aplatt@equalexperts.com>
2017-11-30 13:26:01 +0000

    add tests for main function

---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import sys
+import unittest
+from microdc import main
+
+
+class TestMain(unittest.TestCase):
+
+    def test_main_with_no_options(self):
+        """
+        If no options are given then we expect a valid arguments object
+        """
+        with self.assertRaises(SystemExit) as result:
+            main()
+
+        self.assertEqual(result.exception.code, 2)
+
+    def test_main_with_valid_options(self):
+        """
+        If valid options are given then main should return True
+        """
+        sys.argv = ['microdc',
+                    '--config', 'tests/good_config.yaml',
+                    '--account', 'nonprod',
+                    '--env', 'dev',
+                    '--stack', 'application',
+                    'up']
+        result = main()
+        self.assertEquals(result, True)
